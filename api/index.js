2ed7ee0015ead473dae4788e07730855ceb95476
Gilberto Silva <gilbertoantoniouk@gmail.com>
2023-11-03 19:26:29 +0000

    allow cross origin resource sharing

--- api/index.js
+++ api/index.js
@@ -1,10 +1,12 @@
 /* eslint-disable no-console */
 /* eslint-disable no-unused-vars */
 import express from 'express';
+import cors from 'cors';
 import routes from './routes/index.js';
 import './helpers/redisHelper.js';
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 routes(app);
 
